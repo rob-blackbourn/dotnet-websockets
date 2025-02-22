f994f6181bf8aec0ff99b3a5a28c66b60a250e8d
Rob Blackbourn <rob.blackbourn@gmail.com>
2025-02-22 10:34:59 +0000

    Tests for unicode

--- WebSockets.Core.Test/TestFrames.cs
+++ WebSockets.Core.Test/TestFrames.cs
@@ -44,6 +44,24 @@ namespace WebSockets.Core.Test
                 true);
         }
 
+        [TestMethod]
+        public void TestNonAsciiTextUnmasked()
+        {
+            AssertFrameData(
+                OpCode.Text, Encoding.UTF8.GetBytes("café"),
+                [129, 5, 99, 97, 102, 195, 169],
+                false);
+        }
+
+        [TestMethod]
+        public void TestNonAsciiTextMasked()
+        {
+            AssertFrameData(
+                OpCode.Text, Encoding.UTF8.GetBytes("café"),
+                [129, 133, 100, 190, 238, 126, 7, 223, 136, 189, 205],
+                true);
+        }
+
         private void AssertFrameData(OpCode opCode, byte[] payload, byte[] expected, bool isMasked)
         {
             var mask = isMasked ? expected.Skip(2).Take(4).ToArray() : null;
