7e8e973e369797ea491fc58e3e5c7d46f9fc55fd
Rob Blackbourn <rob.blackbourn@gmail.com>
2025-02-22 18:05:59 +0000

    Added web request

---
+++ WebSockets.Core.Test/TestWebRequest.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Text;
+using WebSockets.Core;
+
+namespace WebSockets.Core.Test
+{
+    [TestClass]
+    public sealed class TestWebRequest
+    {
+        [TestMethod]
+        public void TestParse()
+        {
+            var text =
+                "GET /chat HTTP/1.1\r\n" +
+                "Host: server.example.com\r\n" +
+                "Upgrade: websocket\r\n" +
+                "Connection: Upgrade\r\n" +
+                "Sec-WebSocket-Key: x3JJHMbDL1EzLkh9GBhXDw==\r\n" +
+                "Sec-WebSocket-Protocol: chat, superchat\r\n" +
+                "Sec-WebSocket-Version: 13\r\n" +
+                "Origin: http://example.com\r\n";
+            var webRequest = WebRequest.Parse(text);
+            Assert.AreEqual("GET", webRequest.Verb);
+            Assert.AreEqual("/chat", webRequest.Path);
+            Assert.AreEqual("HTTP/1.1", webRequest.Version);
+            Assert.AreEqual(webRequest.Headers.SingleValue("Host"), "server.example.com");
+            Assert.AreEqual(webRequest.Headers.SingleValue("Upgrade"), "websocket");
+            Assert.AreEqual(webRequest.Headers.SingleValue("Sec-WebSocket-Key"), "x3JJHMbDL1EzLkh9GBhXDw==");
+            Assert.IsTrue(webRequest.Headers.SingleCommaValues("Sec-WebSocket-Protocol")?.SequenceEqual(["chat", "superchat"]));
+            Assert.AreEqual(webRequest.Headers.SingleValue("Sec-WebSocket-Version"), "13");
+            Assert.AreEqual(webRequest.Headers.SingleValue("Origin"), "http://example.com");
+        }
+    }
+}
+
--- WebSockets.Core/ExtensionMethods.cs
+++ WebSockets.Core/ExtensionMethods.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace WebSockets.Core
 {
-    static class ExtensionMethods
+    public static class ExtensionMethods
     {
         public static T[] ToFlatArray<T>(this IList<T[]> buffers)
         {
@@ -33,6 +33,20 @@ namespace WebSockets.Core
             }
 
             return buf;                    
+        }
+
+        public static string? SingleValue(this IDictionary<string, IList<string>> headers, string key)
+        {
+            if (!headers.TryGetValue(key, out var list) || list.Count == 0)
+                return null;
+            if (list.Count > 1)
+                throw new InvalidOperationException("header contains multiple values");
+            return list[0];
+        }
+
+        public static string[]? SingleCommaValues(this IDictionary<string, IList<string>> headers, string key)
+        {
+            return headers.SingleValue(key)?.Split(',', StringSplitOptions.TrimEntries);
         }
     }
 }
