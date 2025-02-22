c0013c5932acba778a725efdd8a3ed1cc53c7a66
Rob Blackbourn <rob.blackbourn@gmail.com>
2025-02-22 10:11:14 +0000

    Added specialized assert to tests

--- WebSockets.Core.Test/TestFrames.cs
+++ WebSockets.Core.Test/TestFrames.cs
@@ -10,18 +10,27 @@ namespace WebSockets.Core.Test
         [TestMethod]
         public void TestTextUnmasked()
         {
-            var frame = new Frame(OpCode.Text, true, Reserved.AllFalse, null, Encoding.UTF8.GetBytes("Spam"));
-            var actual = frame.Serialize();
-            var expected = new byte[] { 129, 4, 83, 112, 97, 109};
-            Assert.IsTrue(actual.SequenceEqual(expected));
+            AssertFrameData(
+                OpCode.Text, Encoding.UTF8.GetBytes("Spam"),
+                [129, 4, 83, 112, 97, 109],
+                false);
         }
 
         [TestMethod]
         public void TestTextMasked()
         {
-            var frame = new Frame(OpCode.Text, true, Reserved.AllFalse, [91, 251, 225, 168], Encoding.UTF8.GetBytes("Spam"));
+            AssertFrameData(
+                OpCode.Text, Encoding.UTF8.GetBytes("Spam"),
+                [129, 132, 91, 251, 225, 168, 8, 139, 128, 197],
+                true
+            );
+        }
+
+        private void AssertFrameData(OpCode opCode, byte[] payload, byte[] expected, bool isMasked)
+        {
+            var mask = isMasked ? expected.Skip(2).Take(4).ToArray() : null;
+            var frame = new Frame(OpCode.Text, true, Reserved.AllFalse, mask, payload);
             var actual = frame.Serialize();
-            byte[] expected = [129, 132, 91, 251, 225, 168, 8, 139, 128, 197];
             Assert.IsTrue(actual.SequenceEqual(expected));
         }
     }
