0023df22b03c949e03933fec0473746d09671679
Rob Blackbourn <rob.blackbourn@gmail.com>
2025-02-24 15:19:36 +0000

    renaming

---
+++ WebSocketServer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebSocketServer
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
